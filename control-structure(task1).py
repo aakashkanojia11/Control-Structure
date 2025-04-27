5b87f2ee04769827d3c3dc24db099e23d0d81872
aakash05 <158532461+aakashkanojia11@users.noreply.github.com>
2025-04-27 05:53:27 +0530

    Add files via upload

---
+++ control-structure(task1).py
@@ -0,0 +1,9 @@
+# input from the user
+num = int(input("Enter the number : "))
+
+# Checking if the num is even or odd ?
+
+if num % 2 == 0:
+    print(f"{num} is an even number.")
+else:
+    print(f"{num} is an odd number.")
---
+++ control-structure(task2).py
@@ -0,0 +1,9 @@
+# Initialize sum variable
+sum = 0
+
+# Iterate over numbers from 1 to 50 and add them to total_sum
+for num in range(1, 51):
+    sum = sum + num
+
+# Display the final sum
+print("The sum of number  from 1 to 50 is:", sum)
